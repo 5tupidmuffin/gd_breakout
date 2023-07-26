71a5754b86414a3563d3b26b6b6eb905396f7547
5tupidmuffin <rameshwark9500@gmail.com>
2023-07-26 08:39:17 +0530

    added default background color

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _ready() -> void:
+	RenderingServer.set_default_clear_color(Color.BLACK)
+
+
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 
